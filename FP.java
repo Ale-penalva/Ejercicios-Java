bcd03dfbd71610cef836b8cddeacd14a55332201
Ale-penalva <alepenalva2@gmail.com>
2024-09-15 22:33:48 +0200

    Add files via upload

---
+++ FP.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+
+public class FP {
+
+	public static void main(String[] args) {
+		
+		double radio;
+		
+		double perimetroCirculo;
+		
+		double perimetroRectangulo;
+		
+		double areaRectangulo;
+		
+		int base;
+		
+		int altura;
+		
+		double cateto1;
+		
+		double cateto2;
+		
+		
+		System.out.println("Vamos a calcular el área de un círculo, para ello necesito que ingreses el radio de este: ");
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		radio=entrada.nextInt();
+		
+		double area=Math.PI*radio*radio;
+		
+		System.out.printf("El área de tu círculo es: %.2f cm. ", area );
+		
+		perimetroCirculo= (2*Math.PI)*radio;
+		
+		System.out.printf("y su perímetro es de: %.2f cm. " , perimetroCirculo);
+		
+		System.out.println( '\n' 
+		+ "Ahora, haremos lo mismo, pero con un rectángulo" + '\n' 
+		+ "Dime el valor de la base del rectángulo:  ");
+		
+		base=entrada.nextInt();
+		
+		System.out.println("Dime ahora, la altura de tu triángulo: ");
+		
+		altura=entrada.nextInt();
+		
+		perimetroRectangulo= 2*(base+altura);
+		
+		areaRectangulo=base*altura; 	
+		
+		System.out.printf("El perímetro de tu rectángulo es: %.2f cm. " ,  perimetroRectangulo);
+		System.out.printf("Y su area es de: %.2f cm." , areaRectangulo);
+		
+		System.out.println("Ahora vamos a usar el teorema de Pitágoras. Dime el valor del cateto 1: ");
+		
+		cateto1=entrada.nextInt();
+		
+		System.out.println("Dime el valor del cateto 2: ");
+		
+		cateto2=entrada.nextInt();
+		
+		double catetos=(cateto1*cateto1)+(cateto2*cateto2);
+		
+		System.out.printf("El valor de la hipotenusa al cuadrado es: %.2f " , catetos );
+		
+		System.out.printf ( "por lo tanto el valor de la hipotenusa es: %.2f " , Math.sqrt(cateto1*cateto1+cateto2*cateto2)); 
+	
+		
+		
+		
+		
+		
+		
+	}
+	
+}
